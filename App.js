dc22c32a5c14e002a563ba78d97dd518124040fb
Udhbhav <21889udbhav@gmail.com>
2023-05-28 17:20:03 +0530

    Create sticky header

--- App.js
+++ App.js
@@ -3,19 +3,23 @@ import React from 'react';
 import Home from './screens/Home';
 import { Text, View, Image, Button, ScrollView } from 'react-native';
 import Header from './screens/Header';
+import Myfeed from './screens/Myfeed';
 
 export default function App() {
   
+  
   return (
+    <>
+    <View className="bg-[#1A1A1A]">
+        <Header/>
+    </View>
     <ScrollView className="flex-1  bg-[#1A1A1A]  ">
+      <Myfeed />
       <StatusBar 
       hidden={true}/>
-      <View className="">
-        <Header/>
-      </View>
       <Home/>
       <Home/>      
     </ScrollView>
-    
+  </>
   );
 }
--- screens/Header.js
+++ screens/Header.js
@@ -3,14 +3,12 @@ import React, { useState, useRef } from 'react';
 import { Text, View, Image, Button, TouchableOpacity, StyleSheet, Pressable, } from 'react-native';
 
 function Header() {
+  
   return (
     <View>
-      <View className="flex-row justify-between text-white mt-3 px-5 sticky">
+      <View className="flex-row justify-between text-white mt-3 px-5 h-14 fixed top-0">
         <Image source={require('../assets/menu.png')} className='w-10 h-10'></Image>
         <Image source={{uri: 'https://reactjs.org/logo-og.png'}} className='w-10 h-10 rounded-full'></Image>
-      </View>
-      <View className='bg-[#1A1A1A] mt-5 px-5'>
-        <Text className="text-3xl font-extralight text-[#E5E5E5]">My Feed</Text>
       </View>
     </View>
   )
