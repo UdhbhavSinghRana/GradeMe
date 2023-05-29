67fb78e89705c6b65aec47fc99dc1b887790254c
Udhbhav <21889udbhav@gmail.com>
2023-05-29 18:22:36 +0530

    Made header appear on swipe up

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Home from './screens/Home';
-import { Text, View, Image, Button, ScrollView } from 'react-native';
+import { Text, View, Image, Button, ScrollView, Animated } from 'react-native';
 import Header from './screens/Header';
 import Myfeed from './screens/Myfeed';
 
 export default function App() {
-  
-  
+  const scrollY = new Animated.Value(0);
+  const diffClamp = Animated.diffClamp(scrollY, 0, 70);
+  const translateY = diffClamp.interpolate({
+    inputRange: [0, 70],
+    outputRange: [0, -70]
+  });
+
   return (
-    <>
-    <View className="bg-[#1A1A1A]">
-        <Header/>
-    </View>
-    <ScrollView className="flex-1  bg-[#1A1A1A]  ">
+  <>
+    
+    <ScrollView className="bg-[#1A1A1A] relative " onScroll={(e) => { scrollY.setValue(e.nativeEvent.contentOffset.y) }}>
       <Myfeed />
       <StatusBar 
       hidden={true}/>
       <Home/>
       <Home/>      
     </ScrollView>
+    <Animated.View className="bg-[#1A1A1A] absolute top-0 right-auto left-0 w-full shadow-2xl shadow-gray-700" style={{transform: [ {translateY: translateY} ]}}>
+      <Header/>
+    </Animated.View>
   </>
   );
 }
--- screens/Header.js
+++ screens/Header.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useRef } from 'react';
 import { Text, View, Image, Button, TouchableOpacity, StyleSheet, Pressable, } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 function Header() {
   
   return (
-    <View>
-      <View className="flex-row justify-between text-white mt-3 px-5 h-14 fixed top-0">
-        <Image source={require('../assets/menu.png')} className='w-10 h-10'></Image>
+    <View className="">
+      <View className="flex-row justify-between items-center text-white mt-3 px-5 h-14  ">
+        <Ionicons name="menu" size={32} color="#E5E5E5" />
         <Image source={{uri: 'https://reactjs.org/logo-og.png'}} className='w-10 h-10 rounded-full'></Image>
       </View>
     </View>
